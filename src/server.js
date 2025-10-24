bde1e6ffa63c01a28de435f2c6ec6a770f3bc677
balles-hub <smobales@gmail.com>
2025-10-24 20:43:42 -0300

    Atualiza API com links de todos os episódios

--- src/server.js
+++ src/server.js
@@ -8,16 +8,16 @@ app.use(cors());
 app.use(express.json());
 
 const episodios = [
-  { id: 1, titulo: "Yu-Gi-Oh! Episódio 1", descricao: "O início do duelo!", url: "https://res.cloudinary.com/dbqhmedpc/video/upload/v1761314600/01.mp4" },
-  { id: 2, titulo: "Yu-Gi-Oh! Episódio 2", descricao: "Continua!", url: "https://res.cloudinary.com/dbqhmedpc/video/upload/v1761314610/02.mp4" },
-  { id: 3, titulo: "Yu-Gi-Oh! Episódio 3", descricao: "Terceiro duelo!", url: "https://res.cloudinary.com/dbqhmedpc/video/upload/v1761314620/03.mp4" },
-  { id: 4, titulo: "Yu-Gi-Oh! Episódio 4", descricao: "Mais ação!", url: "https://res.cloudinary.com/dbqhmedpc/video/upload/v1761314630/04.mp4_koyhni.mp4" },
-  { id: 5, titulo: "Yu-Gi-Oh! Episódio 5", descricao: "Duelo épico!", url: "https://res.cloudinary.com/dbqhmedpc/video/upload/v1761314640/05.mp4" },
-  { id: 6, titulo: "Yu-Gi-Oh! Episódio 6", descricao: "Novos desafios!", url: "https://res.cloudinary.com/dbqhmedpc/video/upload/v1761314650/06.mp4" },
-  { id: 7, titulo: "Yu-Gi-Oh! Episódio 7", descricao: "Tensão máxima!", url: "https://res.cloudinary.com/dbqhmedpc/video/upload/v1761314660/07.mp4" },
-  { id: 8, titulo: "Yu-Gi-Oh! Episódio 8", descricao: "Duelo decisivo!", url: "https://res.cloudinary.com/dbqhmedpc/video/upload/v1761314670/08.mp4" },
-  { id: 9, titulo: "Yu-Gi-Oh! Episódio 9", descricao: "Clímax!", url: "https://res.cloudinary.com/dbqhmedpc/video/upload/v1761314680/09.mp4" },
-  { id: 10, titulo: "Yu-Gi-Oh! Episódio 10", descricao: "Conclusão do arco!", url: "https://res.cloudinary.com/dbqhmedpc/video/upload/v1761314690/10.mp4" },
+   { "id": 1, "titulo": "Episódio 1", "url": "https://res.cloudinary.com/dbqhmedpc/video/upload/v1761314174/01.mp4_hqck2m.mp4" },
+  { "id": 2, "titulo": "Episódio 2", "url": "https://res.cloudinary.com/dbqhmedpc/video/upload/v1761314168/02.mp4_b7hyxc.mp4" },
+  { "id": 3, "titulo": "Episódio 3", "url": "https://res.cloudinary.com/dbqhmedpc/video/upload/v1761314217/03.mp4_irtq7m.mp4" },
+  { "id": 4, "titulo": "Episódio 4", "url": "https://res.cloudinary.com/dbqhmedpc/video/upload/v1761314630/04.mp4_koyhni.mp4" },
+  { "id": 5, "titulo": "Episódio 5", "url": "https://res.cloudinary.com/dbqhmedpc/video/upload/v1761314633/05.mp4_ccvlqh.mp4" },
+  { "id": 6, "titulo": "Episódio 6", "url": "https://res.cloudinary.com/dbqhmedpc/video/upload/v1761314335/06.mp4_j8c7st.mp4" },
+  { "id": 7, "titulo": "Episódio 7", "url": "https://res.cloudinary.com/dbqhmedpc/video/upload/v1761314630/07.mp4_lkx2cz.mp4" },
+  { "id": 8, "titulo": "Episódio 8", "url": "https://res.cloudinary.com/dbqhmedpc/video/upload/v1761314670/08.mp4" },
+  { "id": 9, "titulo": "Episódio 9", "url": "https://res.cloudinary.com/dbqhmedpc/video/upload/v1761341323/09.mp4_pvoqm8.mp4" },
+  { "id": 10, "titulo": "Episódio 10", "url": "https://res.cloudinary.com/dbqhmedpc/video/upload/v1761314173/10.mp4_tjkrba.mp4" }
 ];
 
 app.get("/", (req, res) => res.send("API do Yu-Gi-Oh está rodando!"));
